883859f3a435869e6bc2a9e5934e17086dfbce68
Mustapha Aminu <114070811+mustapha-aminu-abubakar@users.noreply.github.com>
2024-12-02 15:07:49 +0000

    commit 30

--- airflow/dags/generate_synthetic_users_health_metrics.py
+++ airflow/dags/generate_synthetic_users_health_metrics.py
@@ -31,36 +31,80 @@ connection = mysql.connector.connect(
 cursor = connection.cursor()
 
 
-def generate_users():
-        users = [
-            {
-                "first_name": fake.first_name(),
-                "last_name": fake.last_name(),
-                "age": fake.random.randint(22,50),
-                "gender": fake.random.choice(['male', 'female', 'others']),
-                "email": fake.unique.email()
-            }
-            for i in range(5)
-        ]
-        insert_query="""
-        INSERT INTO health_metrics_3.users(first_name, last_name, age, gender, email) VALUES(%s, %s, %s, %s, %s)
-        """
+def generate_users(ti, users_count):
 
-        users_tuples=[(
-            user['first_name'],
-            user['last_name'],
-            user['age'],
-            user['gender'],
-            user['email']
-        ) for user in users]
+    ti.xcom_push(key="users_count", value=users_count)    
+    users = [
+        {
+            "first_name": fake.first_name(),
+            "last_name": fake.last_name(),
+            "age": fake.random.randint(22,50),
+            "gender": fake.random.choice(['male', 'female', 'others']),
+            "email": fake.unique.email()
+        }
+        for i in range(users_count)
+    ]
+    insert_query="""
+    INSERT INTO health_metrics_3.users(first_name, last_name, age, gender, email) VALUES(%s, %s, %s, %s, %s)
+    """
 
-        cursor.executemany(insert_query, users_tuples)
+    users_tuples=[(
+        user['first_name'],
+        user['last_name'],
+        user['age'],
+        user['gender'],
+        user['email']
+    ) for user in users]
 
-        connection.commit()
-        cursor.close()
-        connection.close()
+    cursor.executemany(insert_query, users_tuples)
+
+    connection.commit()
+    cursor.close()
+    connection.close()
 
 
+def generate_metrics(ti):
+
+    users_count = ti.xcom_pull(task_ids= "generate_users", key="users_count")
+    print("users_count in generate_metrics", users_count)
+
+    data = [
+        {
+            "user_id": fake.random.randint(1, 6),
+            "date_time": fake.unique.date_time_between_dates(datetime.today() - timedelta(days=3), datetime.today()),
+            "heart_rate": fake.random.randint(50, 100),  # beats per minute
+            "blood_oxygen": round(fake.random.uniform(95, 100), 1),  # percentage
+            "steps_count": fake.random.randint(0, 20000),  # steps
+            "calories_burned": round(fake.random.uniform(100, 1000), 2),  # kcal
+            "sleep_duration": round(fake.random.uniform(4, 10), 2),  # hours
+            "stress_level": fake.random.randint(1, 10),  # scale of 1 to 10
+            "body_temperature": round(fake.random.uniform(36.0, 37.5), 1),  # Celsius
+            "activity_level": fake.random.choice([0, 0.5, 1])
+        }
+        for _ in range(users_count * 2000)
+    ]
+
+    insert_query = """
+        INSERT INTO health_metrics_3.metrics VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """            
+
+    data_tuples = [(
+                record['user_id'], 
+                record['date_time'], 
+                record['heart_rate'], 
+                record['blood_oxygen'], 
+                record['steps_count'], 
+                record['calories_burned'], 
+                record['sleep_duration'], 
+                record['stress_level'], 
+                record['body_temperature'], 
+                record['activity_level']) for record in data]
+
+    cursor.executemany(insert_query, data_tuples)
+
+    connection.commit()
+    cursor.close()
+    connection.close()
 
 
 with DAG(
@@ -79,6 +123,18 @@ with DAG(
         bash_command= f"sudo mysql -h {mysql_cred['host']} -P {mysql_cred['port']} -u {mysql_cred['username']} -p < health_metrics_3.sql"
     )
 
+    generate_users_task = PythonOperator(
+        task_id= "generate_users",
+        python_callable= generate_users,
+        op_kwargs= {"users_count": 5}
+    )
+
+    generate_metrics_taks = PythonOperator(
+        task_id = "generate_metrics",
+        python_callable= generate_metrics
+    )
+
+    
 
 
 
